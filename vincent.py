90e57b3594f4498ed0e3fb048202f02947f9c924
diggerdu <diggerdu@hotmail.com>
2017-03-27 13:01:16 +0800

    model

--- vincent.py
+++ vincent.py
@@ -1,6 +1,6 @@
 import os
 import tensorflow as tf
-
+import tensorflow.contrib.layers as ly
 import numpy as np
 import time
 import inspect
@@ -9,165 +9,146 @@ VGG_MEAN = [103.939, 116.779, 123.68]
 
 
 class Vgg19:
-    """
-    A trainable version VGG19.
-    """
+    def __init__(self, vgg19_npy_path=None):
+        if vgg19_npy_path is None:
+            path = inspect.getfile(Vgg19)
+            path = os.path.abspath(os.path.join(path, os.pardir))
+            path = os.path.join(path, "vgg19.npy")
+            vgg19_npy_path = path
+            print(vgg19_npy_path)
 
-    def __init__(self, vgg19_npy_path=None, trainable=True):
-        if vgg19_npy_path is not None:
-            self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
-        else:
-            self.data_dict = None
+        self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+        print("npy file loaded")
+        self.init = ly.xavier_initializer_conv2d()
 
-        self.var_dict = {}
-        self.trainable = trainable
-
-    def build(self, rgb, train_mode=None):
+    def build(self, rgb):
         """
         load variable from npy to build the VGG
 
         :param rgb: rgb image [batch, height, width, 3] values scaled [0, 1]
-        :param train_mode: a bool tensor, usually a placeholder: if True, dropout will be turned on
         """
 
+        start_time = time.time()
+        print("build model started")
         rgb_scaled = rgb * 255.0
 
         # Convert RGB to BGR
-        red, green, blue = tf.split(3, 3, rgb_scaled)
+        red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
         assert red.get_shape().as_list()[1:] == [224, 224, 1]
         assert green.get_shape().as_list()[1:] == [224, 224, 1]
         assert blue.get_shape().as_list()[1:] == [224, 224, 1]
-        bgr = tf.concat(3, [
+        bgr = tf.concat(axis=3, values=[
             blue - VGG_MEAN[0],
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
         ])
         assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
-        self.conv1_1 = self.conv_layer(bgr, 3, 64, "conv1_1")
-        self.conv1_2 = self.conv_layer(self.conv1_1, 64, 64, "conv1_2")
+        self.conv1_1 = self.conv_layer(bgr, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
         self.pool1 = self.max_pool(self.conv1_2, 'pool1')
 
-        self.conv2_1 = self.conv_layer(self.pool1, 64, 128, "conv2_1")
-        self.conv2_2 = self.conv_layer(self.conv2_1, 128, 128, "conv2_2")
+        self.conv2_1 = self.conv_layer(self.pool1, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, "conv2_2")
         self.pool2 = self.max_pool(self.conv2_2, 'pool2')
 
-        self.conv3_1 = self.conv_layer(self.pool2, 128, 256, "conv3_1")
-        self.conv3_2 = self.conv_layer(self.conv3_1, 256, 256, "conv3_2")
-        self.conv3_3 = self.conv_layer(self.conv3_2, 256, 256, "conv3_3")
-        self.conv3_4 = self.conv_layer(self.conv3_3, 256, 256, "conv3_4")
+        self.conv3_1 = self.conv_layer(self.pool2, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, "conv3_4")
         self.pool3 = self.max_pool(self.conv3_4, 'pool3')
-
-        self.conv4_1 = self.conv_layer(self.pool3, 256, 512, "conv4_1")
-        self.conv4_2 = self.conv_layer(self.conv4_1, 512, 512, "conv4_2")
-        self.conv4_3 = self.conv_layer(self.conv4_2, 512, 512, "conv4_3")
-        self.conv4_4 = self.conv_layer(self.conv4_3, 512, 512, "conv4_4")
-        self.pool4 = self.max_pool(self.conv4_4, 'pool4')
-
-        self.conv5_1 = self.conv_layer(self.pool4, 512, 512, "conv5_1")
-        self.conv5_2 = self.conv_layer(self.conv5_1, 512, 512, "conv5_2")
-        self.conv5_3 = self.conv_layer(self.conv5_2, 512, 512, "conv5_3")
-        self.conv5_4 = self.conv_layer(self.conv5_3, 512, 512, "conv5_4")
-        self.pool5 = self.max_pool(self.conv5_4, 'pool5')
-
-        self.fc6 = self.fc_layer(self.pool5, 25088, 4096, "fc6")  # 25088 = ((224 / (2 ** 5)) ** 2) * 512
-        self.relu6 = tf.nn.relu(self.fc6)
-        if train_mode is not None:
-            self.relu6 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu6, 0.5), lambda: self.relu6)
-        elif self.trainable:
-            self.relu6 = tf.nn.dropout(self.relu6, 0.5)
-
-        self.fc7 = self.fc_layer(self.relu6, 4096, 4096, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
-        if train_mode is not None:
-            self.relu7 = tf.cond(train_mode, lambda: tf.nn.dropout(self.relu7, 0.5), lambda: self.relu7)
-        elif self.trainable:
-            self.relu7 = tf.nn.dropout(self.relu7, 0.5)
-
-        self.fc8 = self.fc_layer(self.relu7, 4096, 1000, "fc8")
-
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
-
+        self.conv4_1 = self.conv_layer(self.pool3, "conv4_1")
+        self.feature_con, self.feature_sty = tf.unpack(self.conv4_1, axis=0)
+        self.mean, self.var = tf.nn.moments(self.feature_sty, [0, 1, 2])
+        # Adaptive Instance Normalization
+        self.AdaIn = tf.nn.batch_normalization(x=self.feature_con,
+                                               mean=self.mean,
+                                               variance=self.var,
+                                               offset=self.mean,
+                                               scale=self.var,
+                                               variance_epsilon=1e-6
+                                               )
+        # gc
         self.data_dict = None
 
-    def avg_pool(self, bottom, name):
-        return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+        self.tconv4_1 = self.conv_tran_layer(self.AdaIn, 512, 256)
+        self.up3 = self.up_sampling(self.tconv4_1)
+        self.tconv3_4 = self.conv_tran_layer(self.up3, 256, 256)
+        self.tconv3_3 = self.conv_tran_layer(self.tconv3_4, 256, 256)
+        self.tconv3_2 = self.conv_tran_layer(self.tconv3_3, 256, 256)
+        self.tconv3_1 = self.conv_tran_layer(self.tconv3_2, 256, 128)
+        self.up2 = self.up_sampling(self.tconv3_1)
+        self.tconv2_2 = self.conv_tran_layer(self.up2, 128, 128)
+        self.tconv2_1 = self.conv_tran_layer(self.tconv2_2, 128, 64)
+        self.up1 = self.up_sampling(self.tconv2_1)
+        self.tconv2_2 = self.conv_tran_layer(self.tconv2_2, 64, 64)
+        self.tconv2_1 = self.conv_tran_layer(self.tconv2_2, 64, 3)
 
-    def max_pool(self, bottom, name):
-        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+        print(("build model finished: %ds" % (time.time() - start_time)))
+    def up_sampling(self, bottom, name=None):
+        size = self.ten_sh(bottom)
+        return tf.image.resize_nearest_neighbor(bottom, size*2)
+    def avg_pool(self, bottom, name=None):
+        return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME', name=name)
 
-    def conv_layer(self, bottom, in_channels, out_channels, name):
+    def max_pool(self, bottom, name=None):
+        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+    def conv_tran_layer(self, bottom, in_channels, out_channels, name=None):
         with tf.variable_scope(name):
-            filt, conv_biases = self.get_conv_var(3, in_channels, out_channels, name)
+            init = ly.xavier_initializer_conv2d()
+            output = ly.convolution2d_transpose(inputs=bottom,
+                                                num_outputs=out_channels,
+                                                kernel_size=[3, 3],
+                                                stride=[1, 1],
+                                                padding='SAME',
+                                                activation_fn=tf.nn.relu,
+                                                weights_initializer=init,
+                                                bias_initializer=init,
+                                                trainable=True
+                                                )
+
+            return output
+
+    def conv_layer(self, bottom, name):
+        with tf.variable_scope(name):
+            filt = self.get_conv_filter(name)
 
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
-            bias = tf.nn.bias_add(conv, conv_biases)
-            relu = tf.nn.relu(bias)
 
+            conv_biases = self.get_bias(name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
             return relu
 
-    def fc_layer(self, bottom, in_size, out_size, name):
+    def fc_layer(self, bottom, name):
         with tf.variable_scope(name):
-            weights, biases = self.get_fc_var(in_size, out_size, name)
+            shape = bottom.get_shape().as_list()
+            dim = 1
+            for d in shape[1:]:
+                dim *= d
+            x = tf.reshape(bottom, [-1, dim])
 
-            x = tf.reshape(bottom, [-1, in_size])
+            weights = self.get_fc_weight(name)
+            biases = self.get_bias(name)
+
+            # Fully connected layer. Note that the '+' operation automatically
+            # broadcasts biases.
             fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
 
             return fc
 
-    def get_conv_var(self, filter_size, in_channels, out_channels, name):
-        initial_value = tf.truncated_normal([filter_size, filter_size, in_channels, out_channels], 0.0, 0.001)
-        filters = self.get_var(initial_value, name, 0, name + "_filters")
+    def get_conv_filter(self, name):
+        return tf.constant(self.data_dict[name][0], name="filter")
 
-        initial_value = tf.truncated_normal([out_channels], .0, .001)
-        biases = self.get_var(initial_value, name, 1, name + "_biases")
+    def get_bias(self, name):
+        return tf.constant(self.data_dict[name][1], name="biases")
 
-        return filters, biases
+    def get_fc_weight(self, name):
+        return tf.constant(self.data_dict[name][0], name="weights")
 
-    def get_fc_var(self, in_size, out_size, name):
-        initial_value = tf.truncated_normal([in_size, out_size], 0.0, 0.001)
-        weights = self.get_var(initial_value, name, 0, name + "_weights")
-
-        initial_value = tf.truncated_normal([out_size], .0, .001)
-        biases = self.get_var(initial_value, name, 1, name + "_biases")
-
-        return weights, biases
-
-    def get_var(self, initial_value, name, idx, var_name):
-        if self.data_dict is not None and name in self.data_dict:
-            value = self.data_dict[name][idx]
-        else:
-            value = initial_value
-
-        if self.trainable:
-            var = tf.Variable(value, name=var_name)
-        else:
-            var = tf.constant(value, dtype=tf.float32, name=var_name)
-
-        self.var_dict[(name, idx)] = var
-
-        # print var_name, var.get_shape().as_list()
-        assert var.get_shape() == initial_value.get_shape()
-
-        return var
-
-    def save_npy(self, sess, npy_path="./vgg19-save.npy"):
-        assert isinstance(sess, tf.Session)
-
-        data_dict = {}
-
-        for (name, idx), var in self.var_dict.items():
-            var_out = sess.run(var)
-            if not data_dict.has_key(name):
-                data_dict[name] = {}
-            data_dict[name][idx] = var_out
-
-        np.save(npy_path, data_dict)
-        print("file saved", npy_path)
-        return npy_path
-
-    def get_var_count(self):
-        count = 0
-        for v in self.var_dict.values():
-            count += reduce(lambda x, y: x * y, v.get_shape().as_list())
-        return count
+    def ten_sh(self, tensor):
+        return tensor.get_shape().as_list()
